c341769e087440697f1530de26d55d727d4eed88
William Sevigny <wsvgny@gmail.com>
2020-01-18 12:21:56 -0500

    Added world's best iOS app

---
+++ Bicycle/Bicycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bicycle
+//
+//  Created by William Sevigny on 2020-01-18.
+//  Copyright © 2020 William Sevigny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
