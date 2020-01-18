2762fb54ae53092dab5d9ee8b2e96bbd13f523ec
William Sevigny <wsvgny@gmail.com>
2020-01-18 12:45:02 -0500

    Added basic main page

---
+++ Bicycle/Bicycle/Buttons/RoundedCorners.swift
@@ -0,0 +1,17 @@
+//
+//  RoundedCorners.swift
+//  Bicycle
+//
+//  Created by William Sevigny on 2020-01-18.
+//  Copyright © 2020 William Sevigny. All rights reserved.
+//
+
+import UIKit
+
+public class RoundedCorners: UIButton {
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 5;
+    }
+}
