203df9a697827cafc783c3b9df905c085c7e014f
William Sevigny <wsvgny@gmail.com>
2020-01-18 15:37:22 -0500

    Station API call implemented

--- Bicycle/Bicycle/RepairStations/RepairStationService.swift
+++ Bicycle/Bicycle/RepairStations/RepairStationService.swift
@@ -12,29 +12,38 @@ import PromiseKit
 import Alamofire
 import JWTDecode
 
-class StationService {
+class RepairStationService {
     // Singleton
-    static let shared = StationService()
+    static let shared = RepairStationService()
     
 }
     
-extension StationService {
+extension RepairStationService {
     @discardableResult
-    private static func get() -> Promise<Data> {
-        let url: URLConvertible = "http://polypaint.me/api/user/AllCanvas"
-        let headers = [
-            "Authorization": "Bearer " + UserDefaults.standard.string(forKey: "token")!,
-            "Accept": "application/json"
-        ]
+    public static func get() -> Promise<Data> {
         
+        let req = ["lat": 45.510860, "lng":-73.619050, "count":3]
+        
+        let url: URLConvertible = "http://localhost:8080/api/stations"
+
         return Promise { (seal) in
-            Alamofire.request(url, method: .get, encoding: JSONEncoding.default, headers: headers).responseJSON{ (response) in
-                switch response.result {
-                case .success( _):
+            Alamofire.request(url, method: .post, parameters: req, encoding: JSONEncoding.default).validate()
+                .responseString { (response) in
+//                    var results = JSONDecoder().decode([RepairStation].self, from: response.data!)
+                    print(response.data!);
+//                    switch response.result {
+//                    case .success:
                     seal.fulfill(response.data!);
-                case .failure(let error):
-                    seal.reject(error)
-                }
+//            };
+        }
+    }
+}
+    
+    static func getCloseStations() -> Promise<[RepairStation]> {
+        return Promise { seal in
+            RepairStationService.get().done{ (data) in
+                var stations = try JSONDecoder().decode([RepairStation].self, from: data)
+                seal.fulfill(stations)
             }
         }
     }
--- Bicycle/Bicycle/ViewController.swift
+++ Bicycle/Bicycle/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool){
+        RepairStationService.getCloseStations().done { (stations) in
+            print(stations)
+        }
+    }
 
 
 }
