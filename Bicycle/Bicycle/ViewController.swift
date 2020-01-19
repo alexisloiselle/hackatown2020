2188dd6b5f45fed8eddc67d71db8247e9d069315
William Sevigny <wsvgny@gmail.com>
2020-01-18 21:55:19 -0500

    Nothing to see here, move along

--- Bicycle/Bicycle/ViewController.swift
+++ Bicycle/Bicycle/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-
+    
     @IBOutlet weak var repairStationTable: UITableView!
     @IBOutlet weak var repairStationTableTitleLabel: UILabel!
     @IBOutlet weak var helpButton: RoundedCorners!
@@ -33,7 +33,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // For use in foreground
         self.locationManager.requestWhenInUseAuthorization()
         
-        self.initSocket()
+        self.initLocationServices()
         
         let repairStationCellNib = UINib.init(nibName: "RepairStationCell", bundle: nil)
         self.repairStationTable.register(repairStationCellNib, forCellReuseIdentifier: "RepairStationCell")
@@ -43,28 +43,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.helpButton.backgroundColor = hexStringToUIColor(hex: "#32CD32");
     }
     
-    override func viewWillAppear(_ animated: Bool){
-        RepairStationService.getCloseStations().done { (stations) in
-            self.repairStations = stations;
-            self.repairStationTable.reloadData();
-            self.repairStationTableTitleLabel.text = " \(self.repairStations.count) stations nearby";
-        }
-    }
-    
     func hexStringToUIColor (hex:String) -> UIColor {
         var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-
+        
         if (cString.hasPrefix("#")) {
             cString.remove(at: cString.startIndex)
         }
-
+        
         if ((cString.count) != 6) {
             return UIColor.gray
         }
-
+        
         var rgbValue:UInt64 = 0
         Scanner(string: cString).scanHexInt64(&rgbValue)
-
+        
         return UIColor(
             red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
             green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
@@ -73,18 +65,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         )
     }
     
-    override func viewWillAppear(_ animated: Bool){
-        self.fetchNearStations()
-    }
-    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let locValue:CLLocationCoordinate2D = manager.location!.coordinate
         print("locations = \(locValue.latitude) \(locValue.longitude)")
+        SocketService.updatePosition(lat: locValue.latitude, lng: locValue.longitude)
+        
+        if CLLocationManager.locationServicesEnabled() {
+            if (CLLocationManager.authorizationStatus() == .authorizedAlways || CLLocationManager.authorizationStatus() == .authorizedWhenInUse) {
+                if (!SocketService.initialized) {
+                    self.initSocket()
+                    SocketService.initialized = true
+                } else {
+                    SocketService.updatePosition(lat: locValue.latitude, lng: locValue.longitude)
+                }
+                self.fetchNearStations()
+            }
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        self.initSocket()
-        self.fetchNearStations()
+        self.initLocationServices()
     }
     
     func fetchNearStations() -> Void {
@@ -99,20 +99,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    func initSocket() -> Void {
+    func initLocationServices() -> Void {
         if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
             if (CLLocationManager.authorizationStatus() == .authorizedAlways || CLLocationManager.authorizationStatus() == .authorizedWhenInUse) {
-                // Connect to socket
-                SocketService.start(lat: locationManager.location!.coordinate.latitude, lng: locationManager.location!.coordinate.longitude)
-                
-                Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { timer in
-                    SocketService.updatePosition(lat: self.locationManager.location!.coordinate.latitude, lng: self.locationManager.location!.coordinate.longitude)
-                }
+                locationManager.delegate = self
+                locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+                locationManager.startUpdatingLocation()
             }
         }
+    }
+    
+    func initSocket() -> Void {
+        // Connect to socket
+        SocketService.start(lat: locationManager.location!.coordinate.latitude, lng: locationManager.location!.coordinate.longitude, viewController: self)
     }
 }
 
@@ -127,7 +126,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RepairStationCell", for: indexPath) as! RepairStationCell
-
+        
         cell.stationOperatorLabel.text = "\(self.repairStations[indexPath.row].stationOperator)";
         cell.distanceLabel.text = "\(self.repairStations[indexPath.row].distance) m"
         
@@ -136,7 +135,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if #available(iOS 13.0, *) {
-            if let repairStationMapController = storyboard?.instantiateViewController(identifier: "RepairStationMapController") as? RepairStationsMapsViewController {
+            if let repairStationMapController = storyboard?.instantiateViewController(withIdentifier: "RepairStationMapController") as? RepairStationsMapsViewController {
                 repairStationMapController.lat = "caliss"
                 navigationController?.pushViewController(repairStationMapController, animated: true)
             }
